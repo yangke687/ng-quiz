28dba19affad79ce099483cf929881f4a3e8c0e8
Yang Ke <saint_yk@163.com>
2017-04-02 16:05:57 +0800

    show question answers when reporting quiz results

--- js/controllers/resultController.js
+++ js/controllers/resultController.js
@@ -1,7 +1,15 @@
 module.controller('resultController', ['quizMetrics', 'dataService', '$scope', function(quizMetrics, dataService, $scope) {
 	$scope.quizMetrics = quizMetrics;
 	$scope.dataService = dataService;
+	$scope.activeQuestion = 0;
 	$scope.setActiveQuestion = function(idx) {
-		console.log(idx);
-	}
+		$scope.activeQuestion = idx;
+	};
+	$scope.getAnswerClass = function(idx) {
+		if (idx === quizMetrics.correctAnswers[$scope.activeQuestion]) {
+			return 'bg-success';
+		} else if (idx === dataService.quizQuestions[$scope.activeQuestion].selected) {
+			return 'bg-danger';
+		}
+	};
 }]);
